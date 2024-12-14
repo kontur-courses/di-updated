64037f8469d0584f886ecd22744b50edc68786b8
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2024-12-14 20:33:56 +0500

    added word statistics

---
+++ TagCloud/WordStatistics/WordStatisticsImpl.cs
@@ -0,0 +1,29 @@
+namespace TagCloud.WordStatistics;
+
+public class WordStatisticsImpl : IWordStatistics
+{
+    private readonly Dictionary<string, int> _wordCounts = new();
+    private int _totalWordCount = 0;
+    
+    public double GetWordFrequency(string word)
+    {
+        return _wordCounts.TryGetValue(word, out var count)
+            ? (double)count / _totalWordCount
+            : 0;
+    }
+
+    public IEnumerable<string> GetWords()
+    {
+        return _wordCounts.Keys;
+    }
+
+    public void Populate(IEnumerable<string> words)
+    {
+        foreach (var word in words)
+        {
+            _wordCounts.TryAdd(word, 0);
+            _wordCounts[word]++;
+            _totalWordCount++;
+        }
+    }
+}
