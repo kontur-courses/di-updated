1a8ea9e98155e9b4b031514ff7dfb75fcc06234b
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2024-12-14 20:16:41 +0500

    small refactoring of IWordProcessor and TagProcessor

--- TagCloud/WordPreprocessor/IWordPreprocessor.cs
+++ TagCloud/WordPreprocessor/IWordPreprocessor.cs
@@ -3,5 +3,6 @@ namespace TagCloud.WordPreprocessor;
 public interface IWordPreprocessor
 {
     public IEnumerable<string> ExtractWords(string text);
-    public string? ProcessWord(string word);
+    public string ProcessWord(string word);
+    public bool IsGoodWord(string word);
 }
--- TagCloud/WordPreprocessor/TagPreprocessor.cs
+++ TagCloud/WordPreprocessor/TagPreprocessor.cs
@@ -11,15 +11,17 @@ public class TagPreprocessor(
     {
         return text.Split(_delimiters,
                 StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
-            .Distinct()
             .Select(ProcessWord)
-            .Where(word => word != null)!;
+            .Where(IsGoodWord);
     }
 
-    public string? ProcessWord(string word)
+    public string ProcessWord(string word)
     {
-        return boringWordProvider.IsBoring(word)
-            ? word.ToLower()
-            : null;
+        return word.ToLower();
+    }
+
+    public bool IsGoodWord(string word)
+    {
+        return !boringWordProvider.IsBoring(word);
     }
 }
