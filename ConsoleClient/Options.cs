a0d4dca402b5302ddbe2d898a0061092b7e9baeb
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2024-12-17 22:31:57 +0500

    added -boring and -delimiters options

--- ConsoleClient/Options.cs
+++ ConsoleClient/Options.cs
@@ -9,4 +9,10 @@ public class Options
     
     [Option('o', "output", Required = true, HelpText = "Output file path.")]
     public string OutputFile { get; set; }
+    
+    [Option('d', "delimiters", Required = false, Default = null, HelpText = "Path to file with word delimiters.")]
+    public string WordDelimiterFile { get; set; }
+    
+    [Option('b', "boring", Required = false, Default = null, HelpText = "Path to file with boring words.")]
+    public string BoringWordsFile { get; set; }
 }
