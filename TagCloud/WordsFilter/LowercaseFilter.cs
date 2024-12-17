aa27044bb5f0598c1b05e8defb593b130d414399
Geratoptus <shura.bocharov.2004@mail.ru>
2024-12-17 20:03:38 +0500

    feat(LowerCaseFilter.cs): написал фильтр на регистр

--- TagCloud/WordsFilter/LowercaseFilter.cs
+++ TagCloud/WordsFilter/LowercaseFilter.cs
@@ -1,6 +1,9 @@
 namespace TagCloud.WordsFilter;
 
-public class LowercaseFilter
+public class LowercaseFilter : IWordsFilter
 {
-    
+    public List<string> ApplyFilter(List<string> words) 
+        => words
+            .Select(w => w.ToLower())
+            .ToList();
 }
