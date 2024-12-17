d9d68183f962745bc203b7bd80939df3fcd8d0d2
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2024-12-17 22:32:31 +0500

    added ability to load word delimiters from a file

--- TagCloud/WordPreprocessor/WordDelimiterProviderImpl.cs
+++ TagCloud/WordPreprocessor/WordDelimiterProviderImpl.cs
@@ -1,11 +1,42 @@
+using TagCloud.FileReader;
+using TagCloud.Logger;
+
 namespace TagCloud.WordPreprocessor;
 
 public class WordDelimiterProviderImpl : IWordDelimiterProvider
 {
-    private readonly List<string> _delimiters = new();
+    private static readonly string _delimitersFilePath = "delimiters.txt";
+    
+    private readonly List<string> _delimiters;
+    private readonly ILogger _logger;
+    private readonly FileReaderRegistry _fileReaderRegistry;
+
+    public WordDelimiterProviderImpl(FileReaderRegistry fileReaderRegistry, ILogger logger)
+    {
+        _fileReaderRegistry = fileReaderRegistry;
+        _logger = logger;
+        _delimiters = new List<string>();
+        _delimiters.AddRange(new []{ "\n", "\t", "\r", " " });
+    }
     
     public string[] GetDelimiters()
     {
+        if (!Path.Exists(_delimitersFilePath))
+        {
+            _logger.Info($"Could not find delimiters.txt file at: {Path.GetFullPath(_delimitersFilePath)}");
+            _logger.Info("Using only default word delimiters.");
+            return _delimiters.ToArray();
+        }
+        
+        if (_fileReaderRegistry.TryGetFileReader(".txt", out var fileReader))
+        {
+            _logger.Info("Detected delimiters.txt file.");
+            fileReader.OpenFile(Path.GetFullPath(_delimitersFilePath));
+            while (fileReader.TryGetNextLine(out var line))
+                _delimiters.Add(line);
+            fileReader.Dispose();
+        }
+        
         return _delimiters.ToArray();
     }
 
