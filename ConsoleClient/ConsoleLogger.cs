f5aa52bb4254b580d9b45d14a95e6793482ad733
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2024-12-17 21:39:13 +0500

    added report progress functionality to ILogger

--- ConsoleClient/ConsoleLogger.cs
+++ ConsoleClient/ConsoleLogger.cs
@@ -11,11 +11,37 @@ public class ConsoleLogger : ILogger
 
     public void Warning(string message)
     {
-        Console.WriteLine($"WARNING: {message}", ConsoleColor.Yellow);
+        var oldColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"WARNING: {message}");
+        Console.ForegroundColor = oldColor;
     }
 
     public void Error(string message)
     {
-        Console.WriteLine($"ERROR: {message}", ConsoleColor.Red);
+        var oldColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"ERROR: {message}");
+        Console.ForegroundColor = oldColor;
+    }
+
+    public void ReportProgress(string message, double progress)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(progress);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(progress, 1);
+        var progressPercentage = (int)(progress * 100.0);
+        var (left, top) = Console.GetCursorPosition();
+        Console.Write($"{message}: [");
+        for (var i = 0; i <= 10; i++)
+        {
+            if (i <= progressPercentage / 10)
+                Console.Write('=');
+            else
+                Console.Write(' ');
+        }
+        Console.WriteLine(']');
+        
+        if (progressPercentage < 100)
+            Console.SetCursorPosition(left, top);
     }
 }
