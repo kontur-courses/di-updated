4a9234e638080c250fe0d752ca1414b6d0febfca
Geratoptus <shura.bocharov.2004@mail.ru>
2024-12-17 19:26:59 +0500

    add(FileReader.cs): реализовал FileReader

--- TagCloud/WordsReader/Readers/FileReader.cs
+++ TagCloud/WordsReader/Readers/FileReader.cs
@@ -1,9 +1,12 @@
+using System.Text;
+
 namespace TagCloud.WordsReader.Readers;
 
-public class FileReader : IWordsReader
+public class FileReader(string path, Encoding encoding) : IWordsReader
 {
-    public List<string> ReadWords()
-    {
-        throw new NotImplementedException();
-    }
+    public List<string> ReadWords() 
+        => File.ReadAllLines(path, encoding)
+            .Select(line => line.Split(" "))
+            .SelectMany(arr => arr)
+            .ToList();
 }
