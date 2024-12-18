f5326109734508adc90110f6969c29111c0723a8
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-19 00:31:50 +0500

    Добавил класс для регистрации зависимостей

---
+++ TagsCloudVisualization/Di/DiContainer.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+
+namespace TagsCloudVisualization.Di;
+
+public class DiContainer
+{
+    public static IContainer Configure()
+    {
+        var builder = new ContainerBuilder();
+        
+        // Здесь будут зарегистрированы все зависимости
+        
+        return builder.Build();
+    }
+}
