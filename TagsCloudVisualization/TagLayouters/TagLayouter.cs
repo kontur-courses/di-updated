35a611a6b380809ef2493b5381af97c8aaf787d0
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-19 00:05:27 +0500

    Добавил интерфейс и класс раскладки тегов

---
+++ TagsCloudVisualization/TagLayouters/TagLayouter.cs
@@ -0,0 +1,22 @@
+﻿using TagsCloudVisualization.CircularCloudLayouters;
+using TagsCloudVisualization.Tags;
+using TagsCloudVisualization.TextHandlers;
+
+namespace TagsCloudVisualization.TagLayouters;
+
+public class TagLayouter : ITagLayouter
+{
+    private readonly ICircularCloudLayouter circularCloudLayouter;
+    private readonly ITextHandler textHandler;
+
+    public TagLayouter(ICircularCloudLayouter circularCloudLayouter, ITextHandler textHandler)
+    {
+        this.textHandler = textHandler;
+        this.circularCloudLayouter = circularCloudLayouter;
+    }
+
+    public IEnumerable<Tag> GetTags()
+    {
+        throw new NotImplementedException();
+    }
+}
