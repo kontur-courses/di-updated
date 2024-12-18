6c4c79194a043f817a77a234d38b03a7d774e58f
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-19 00:26:36 +0500

    Добавил класс создания облака

---
+++ TagsCloudVisualization/TagCloudCreator.cs
@@ -0,0 +1,21 @@
+﻿using TagsCloudVisualization.TagLayouters;
+using TagsCloudVisualization.Visualization;
+
+namespace TagsCloudVisualization;
+
+public class TagCloudCreator
+{
+    private readonly ITagLayouter tagLayouter;
+    private readonly IImageDrawer imageDrawer;
+
+    public TagCloudCreator(ITagLayouter tagLayouter, IImageDrawer imageDrawer)
+    {
+        this.tagLayouter = tagLayouter;
+        this.imageDrawer = imageDrawer;
+    }
+
+    public void CreateImage()
+    {
+        throw new NotImplementedException();
+    }
+}
