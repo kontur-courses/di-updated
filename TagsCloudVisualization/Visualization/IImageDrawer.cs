040f8adb578dcf476d5b68511e8602db2572703e
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-19 00:22:16 +0500

    Добавил интерфейс отрисовки изображения

---
+++ TagsCloudVisualization/Visualization/IImageDrawer.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+using TagsCloudVisualization.Tags;
+
+namespace TagsCloudVisualization.Visualization;
+
+public interface IImageDrawer
+{
+    Bitmap Draw(IEnumerable<Tag> tags);
+}
