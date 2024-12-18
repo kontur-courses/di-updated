cc2d9ec090c9d584585db9c664111a0f8d07ad0d
Ilfir Sakhabutdinov <ilfir.sakhabutdinov.study@mail.ru>
2024-12-18 18:04:45 +0500

    Add. Options консольного клиента

---
+++ TagsCloudContainer.ConsoleUi/Options/ImageSettingsOptions.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using CommandLine;
+using TagsCloudContainer.ConsoleUi.Options.Interfaces;
+
+namespace TagsCloudContainer.ConsoleUi.Options;
+
+[Verb("image", HelpText = "Настройка изображения")]
+public class ImageSettingsOptions : IOptions
+{
+    [Option('w', "width", HelpText = "Ширина")]
+    public int Width { get; set; }
+
+    [Option('h', "height", HelpText = "Высота")]
+    public int Height { get; set; }
+
+    [Option('e', "extension", HelpText = "формат файла")]
+    public ImageFormat ImageFormat { get; set; }
+
+    [Option('b', "background", HelpText = "Цвет фона")]
+    public Color BackgroundColor { get; set; }
+
+    [Option('c', "color", HelpText = "Цвет слов")]
+    public Color WordColor { get; set; }
+
+    [Option('f', "font", HelpText = "Шрифт")]
+    public FontFamily FontFamily { get; set; }
+}
---
+++ TagsCloudContainer.ConsoleUi/Options/VisualizationOptions.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine;
+using TagsCloudContainer.ConsoleUi.Options.Interfaces;
+
+namespace TagsCloudContainer.ConsoleUi.Options;
+
+[Verb("files", HelpText = "Настройки путей файлов")]
+public class VisualizationOptions : IOptions
+{
+    [Option('i', "input", Required = true, HelpText = "Путь к файлу текста для анализа.")]
+    public string InputPath { get; set; }
+
+    [Option('o', "output", Required = true, HelpText = "Путь к сохранению изображения.")]
+    public string OutputPath { get; set; }
+}
