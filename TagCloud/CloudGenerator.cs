ba7c531680ab4bb03abe2075741706d69783a86b
Geratoptus <shura.bocharov.2004@mail.ru>
2024-12-18 18:04:24 +0500

    feat(CloudGenerator.cs): работающий CloudGenerator

--- TagCloud/CloudGenerator.cs
+++ TagCloud/CloudGenerator.cs
@@ -1,14 +1,37 @@
 using TagCloud.WordsFilter;
 using TagCloud.WordsReader;
 using TagCloud.ImageGenerator;
+using TagCloud.ImageSaver;
+
 namespace TagCloud;
 
 public class CloudGenerator(
+    IImageSaver saver,
     IWordsReader reader,
-    List<IWordsFilter> filters,
     BitmapGenerator imageGenerator,
-    CloudGeneratorSettings settings)
+    IEnumerable<IWordsFilter> filters)
 {
+    private const int MinFontSize = 10;
+    private const int MaxFontSize = 80;
     public string GenerateTagCloud()
-        => string.Empty;
+    {
+        var words = reader.ReadWords();
+
+        var freqDict = filters
+            .Aggregate(words, (c, f) => f.ApplyFilter(c))
+            .GroupBy(w => w)
+            .OrderByDescending(g => g.Count())
+            .ToDictionary(g => g.Key, g => g.Count());
+        
+        var maxFreq = freqDict.Values.Max();
+        var tagsList = freqDict.Select(pair => ToWordTag(pair, maxFreq)).ToList();
+
+        return saver.Save(imageGenerator.GenerateWindowsBitmap(tagsList));
+    }
+
+    private static int TransformFreqToSize(int freq, int maxFreq) 
+        => (int)(MinFontSize + (float)freq / maxFreq * (MaxFontSize - MinFontSize));
+
+    private static WordTag ToWordTag(KeyValuePair<string, int> pair, int maxFreq)
+        => new(pair.Key, TransformFreqToSize(pair.Value, maxFreq));
 }
