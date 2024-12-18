f5426c1a66f108c929407820e2cd7ada6567d816
S4MPAI <flekshum@gmail.com>
2024-12-19 03:06:21 +0500

    add TagsCloudImageCreator.cs

---
+++ TagsCloudVisualization/TagsCloudImageCreator.cs
@@ -0,0 +1,37 @@
+using System.Text.RegularExpressions;
+using TagsCloudVisualization.ImageSavers;
+using TagsCloudVisualization.Layouters;
+using TagsCloudVisualization.TextReaders;
+using TagsCloudVisualization.Visualizers;
+using TagsCloudVisualization.WordsHandlers;
+
+namespace TagsCloudVisualization;
+
+public class TagsCloudImageCreator(
+    ITextReader[] textReaders,
+    IWordHandler[] wordHandlers,
+    ITagLayouter tagLayouter,
+    ITagVisualizer visualizer,
+    IImageSaver imageSaver)
+{
+    private static readonly Regex GetWordsRegex = new(@"\b[a-zA-Zа-яА-ЯёЁ]+\b", RegexOptions.Compiled);
+    
+    public void CreateImageWithTags(string pathToText)
+    {
+        var text = textReaders
+            .First(x => x.IsCanRead(pathToText))
+            .ReadWords(pathToText);
+        var words = DivideOnWords(text);
+
+        foreach (var wordHandler in wordHandlers)
+            words = wordHandler.Handle(words);
+        
+        var tags = tagLayouter.GetTags(words);
+        using var bitmap = visualizer.Visualize(tags);
+        
+        imageSaver.Save(bitmap);
+    }
+
+    private IEnumerable<string> DivideOnWords(string text) =>
+        GetWordsRegex.Matches(text).Select(x => x.Value);
+}
