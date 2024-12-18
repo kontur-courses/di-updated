d3ff1b0edfafc28db23d6cfc121494f050128088
Geratoptus <shura.bocharov.2004@mail.ru>
2024-12-18 16:51:56 +0500

    fix(CircularCloudLayouter): понял, что для этой задачи лучше начинать поиск каждый раз с центра раскладки

--- TagCloud/CloudLayouter/PointLayouter/CircularCloudLayouter.cs
+++ TagCloud/CloudLayouter/PointLayouter/CircularCloudLayouter.cs
@@ -9,9 +9,8 @@ public class CircularCloudLayouter(Point layoutCenter, IPointsGenerator pointsGe
 {
     private const string FiniteGeneratorExceptionMessage =
         "В конструктор CircularCloudLayouter был передан конечный генератор точек";
-    private readonly IEnumerator<Point> _pointEnumerator = pointsGenerator
-        .GeneratePoints(layoutCenter)
-        .GetEnumerator();
+
+    private readonly List<Point> _placedPoints = [];
     private readonly List<Rectangle> _layoutRectangles = [];
 
     public CircularCloudLayouter(Point layoutCenter, double radius, double angleOffset) :
@@ -26,15 +25,17 @@ public class CircularCloudLayouter(Point layoutCenter, IPointsGenerator pointsGe
 
     public Rectangle PutNextRectangle(Size rectangleSize)
     {
-        var rectangle = _pointEnumerator
-            .ToIEnumerable()
+        var rectangle = pointsGenerator
+            .GeneratePoints(layoutCenter)
+            .Except(_placedPoints)
             .Select(point => new Rectangle()
                 .CreateRectangleWithCenter(point, rectangleSize))
             .FirstOrDefault(rectangle => !_layoutRectangles.Any(rectangle.IntersectsWith));
 
         if (rectangle.IsEmpty)
             throw new InvalidOperationException(FiniteGeneratorExceptionMessage);
-
+        
+        _placedPoints.Add(rectangle.Location - rectangleSize / 2);
         _layoutRectangles.Add(rectangle);
         return rectangle;
     }
