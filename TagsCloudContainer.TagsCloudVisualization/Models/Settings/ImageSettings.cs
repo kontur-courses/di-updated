01469e53907c2ed7db2fd271c0454bda30efee7f
Ilfir Sakhabutdinov <ilfir.sakhabutdinov.study@mail.ru>
2024-12-18 17:57:33 +0500

    Add. Модели для работы с визуализацией слов

---
+++ TagsCloudContainer.TagsCloudVisualization/Models/Settings/ImageSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace TagsCloudContainer.TagsCloudVisualization.Models.Settings;
+
+public record ImageSettings
+{
+    private Size size = new Size(500, 500);
+
+    public Size Size
+    {
+        get => size;
+        init
+        {
+            if (value.Width <= 0 || value.Height <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "Width and Height must be positive.");
+            }
+
+            size = value;
+        }
+    }
+
+    public ImageFormat ImageFormat { get; init; } = ImageFormat.Png;
+    public Color BackgroundColor { get; init; } = Color.White;
+    public Color WordColor { get; init; } = Color.Black;
+    public FontFamily FontFamily { get; init; } = new("Consolas");
+}
---
+++ TagsCloudContainer.TagsCloudVisualization/Models/ViewWord.cs
@@ -0,0 +1,5 @@
+﻿using System.Drawing;
+
+namespace TagsCloudContainer.TagsCloudVisualization.Models;
+
+public record ViewWord(string Word, Font Font);
