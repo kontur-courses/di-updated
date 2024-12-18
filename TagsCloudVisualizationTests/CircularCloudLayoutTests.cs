fd9b4ec80379afbc2623a1c8286797f9480ae91a
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-19 00:46:09 +0500

    Перенес тесты с дз 1

---
+++ TagsCloudVisualizationTests/CircularCloudLayoutTests.cs
@@ -0,0 +1,93 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using FluentAssertions;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using TagsCloudVisualization.CircularCloudLayouters;
+using TagsCloudVisualization.LayoutAlgorithms;
+using TagsCloudVisualization.Visualization;
+using TagsCloudVisualizationTests.Utils;
+
+namespace TagsCloudVisualizationTests;
+
+[TestFixture]
+public class CircularCloudLayoutTests
+{
+    private ICircularCloudLayouter cloudLayouter;
+    private List<Rectangle> addedRectangles;
+    
+    [SetUp]
+    public void Setup()
+    {
+        var center = new Point(0, 0);
+        cloudLayouter = new CircularCloudLayouter(new CircularLayoutAlgorithm(center));
+        addedRectangles = [];
+    }
+
+    // Нужно переделать, так как изменился контракт отрисовки изображения
+    
+    // [TearDown]
+    // public void TearDown()
+    // {
+    //     if (TestContext.CurrentContext.Result.Outcome.Status != TestStatus.Failed) 
+    //         return;
+    //     
+    //     var pathImageStored = TestContext.CurrentContext.TestDirectory + @"\imageFailedTests";
+    //
+    //     if (!Directory.Exists(pathImageStored))
+    //     {
+    //         Directory.CreateDirectory(pathImageStored);
+    //     }
+    //
+    //     var testName = TestContext.CurrentContext.Test.Name;
+    //     
+    //     var bitmap = ImageDrawer.DrawLayout(addedRectangles, 10);
+    //         
+    //     ImageSaver.Save(bitmap, pathImageStored, $"{testName}.png", ImageFormat.Png);
+    //     
+    //     Console.WriteLine($@"Tag cloud visualization saved to file {pathImageStored}\{testName}.png");
+    // }
+    
+    [TestCase(10, 5, 15)]
+    [TestCase(50, 30, 100)]
+    [TestCase(100, 5, 50)]
+    public void PutNextRectangle_ShouldAddedRectanglesDoNotIntersect(int countRectangles, int minSideLength,
+        int maxSideLength)
+    {
+        var rectangleSizes = GeometryUtils.GenerateRectangleSizes(countRectangles, minSideLength, maxSideLength);
+        
+        addedRectangles.AddRange(rectangleSizes.Select(t => cloudLayouter.PutNextRectangle(t)));
+
+        for (var i = 0; i < addedRectangles.Count-1; i++)
+        {
+            addedRectangles
+                .Skip(i + 1)
+                .Any(addedRectangle => addedRectangle.IntersectsWith(addedRectangles[i]))
+                .Should()
+                .BeFalse();
+        }
+    }
+    
+    [TestCase(10, 5, 15)]
+    [TestCase(50, 30, 100)]
+    [TestCase(100, 5, 50)]
+    public void CircleShape_ShouldBeCloseToCircle_WhenAddMultipleRectangles(int countRectangles, int minSideLength, 
+        int maxSideLength)
+    {
+        var rectangleSizes = GeometryUtils.GenerateRectangleSizes(countRectangles, minSideLength, maxSideLength);
+        
+        addedRectangles.AddRange(rectangleSizes.Select(t => cloudLayouter.PutNextRectangle(t)));
+
+        var distances = addedRectangles
+            .Select(rectangle => 
+                GeometryUtils.CalculateDistanceBetweenCenterRectangleAndCenterCloud(rectangle, new Point(0, 0)))
+            .ToArray();
+
+        for (var i = 1; i < distances.Length; i++)
+        {
+            var distanceBetweenRectangles =
+                GeometryUtils.CalculateDistanceBetweenRectangles(addedRectangles[i], addedRectangles[i - 1]);
+            distances[i].Should().BeApproximately(distances[i - 1], distanceBetweenRectangles);
+        }
+    }
+}
---
+++ TagsCloudVisualizationTests/CircularLayoutAlgorithmTests.cs
@@ -0,0 +1,91 @@
+﻿using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudVisualization.LayoutAlgorithms;
+using TagsCloudVisualizationTests.Utils;
+
+namespace TagsCloudVisualizationTests;
+
+[TestFixture]
+public class CircularLayoutAlgorithmTests
+{
+    [TestCase(0)]
+    [TestCase(-5)]
+    public void Constructor_ShouldArgumentException_WhenStepIncreasingRadiusHasInvalidValue(int stepIncreasingRadius)
+    {
+        var action = () =>
+        {
+            _ = new CircularLayoutAlgorithm(new Point(0, 0), stepIncreasingRadius: stepIncreasingRadius);
+        };
+
+        action
+            .Should()
+            .Throw<ArgumentException>()
+            .WithMessage("The parameter 'stepIncreasingRadius' is less than or equal to zero");
+    }
+
+    [Test]
+    public void Constructor_ShouldArgumentException_WhenStepIncreasingAngleIsZero()
+    {
+        var action = () =>
+        {
+            _ = new CircularLayoutAlgorithm(new Point(0, 0), stepIncreasingAngle: 0);
+        };
+
+        action
+            .Should()
+            .Throw<ArgumentException>()
+            .WithMessage("The parameter 'stepIncreasingAngle' is zero");
+    }
+
+    [TestCase(0, 0)]
+    [TestCase(-4, 5)]
+    public void CalculateNextPoint_ShouldPointIsCenter_WhenCalculateFirstPoint(int centerCoordinateX, int centerCoordinateY)
+    {
+        var center = new Point(centerCoordinateY, centerCoordinateY);
+        var circularLayoutAlgorithm = new CircularLayoutAlgorithm(center);
+
+        var nextPoint = circularLayoutAlgorithm.CalculateNextPoint();
+
+        nextPoint.Should().Be(center);
+    }
+
+    [TestCase(2)]
+    [TestCase(7)]
+    public void CalculateNextPoint_ShouldIncreaseRadius_WhenCalculateTwoPoints(int stepIncreasingRadius)
+    {
+        var circularLayoutAlgorithm =
+            new CircularLayoutAlgorithm(new Point(0, 0), stepIncreasingRadius: stepIncreasingRadius);
+
+        var firstPoint = circularLayoutAlgorithm.CalculateNextPoint();
+        var secondPoint = circularLayoutAlgorithm.CalculateNextPoint();
+        var distanceBetweenPoints = GeometryUtils.CalculateDistanceBetweenPoints(firstPoint, secondPoint);
+        
+        distanceBetweenPoints.Should().Be(stepIncreasingRadius);
+    }
+
+    [TestCase(Math.PI / 4)]
+    [TestCase(Math.PI / 2)]
+    public void CalculateNextPoint_ShouldIncreaseAngle_WhenCalculateThreePoints(double stepIncreasingAngle)
+    {
+        var stepIncreasingRadius = 2;
+        var center = new Point(0, 0);
+        var circularLayoutAlgorithm = new CircularLayoutAlgorithm(
+            center,
+            stepIncreasingRadius: stepIncreasingRadius,
+            stepIncreasingAngle: stepIncreasingAngle
+            );
+        
+        _ = circularLayoutAlgorithm.CalculateNextPoint();
+        var secondPoint = circularLayoutAlgorithm.CalculateNextPoint();
+        var thirdPoint = circularLayoutAlgorithm.CalculateNextPoint();
+        var distanceBetweenCenterAndSecondPoint = GeometryUtils.CalculateDistanceBetweenPoints(center, secondPoint);
+        var distanceBetweenCenterAndThirdPoint = GeometryUtils.CalculateDistanceBetweenPoints(center, thirdPoint);
+        
+        // проверяем, что точки не равны
+        secondPoint.Should().NotBe(thirdPoint);
+        // проверяем, что точки в пределах круга с одним радиусом
+        distanceBetweenCenterAndSecondPoint.Should().BeLessThanOrEqualTo(stepIncreasingRadius);
+        distanceBetweenCenterAndThirdPoint.Should().BeLessThanOrEqualTo(stepIncreasingRadius);
+    }
+}
