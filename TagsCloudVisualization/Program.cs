b9e717c9a4a82751d9956d5e6154d1390a6c9cc9
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-19 00:35:36 +0500

    Наметки в Program

--- TagsCloudVisualization/Program.cs
+++ TagsCloudVisualization/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Drawing.Imaging;
+using Autofac;
+using TagsCloudVisualization;
+using TagsCloudVisualization.Di;
+using TagsCloudVisualization.Visualization;
+
+var container = DiContainer.Configure();
+var cloudCreator = container.Resolve<TagCloudCreator>();
+
+var image = cloudCreator.CreateImage();
+
+ImageSaver.Save(image, "", "", ImageFormat.Png);
 
 Console.WriteLine("Hello, World!");
