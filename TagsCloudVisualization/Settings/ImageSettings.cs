02c81bd13078d76355942d08490bf4b1179aa984
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-19 00:21:49 +0500

    Добавил класс настроек изображения

---
+++ TagsCloudVisualization/Settings/ImageSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization.Settings;
+
+public class ImageSettings
+{
+    public ImageSettings(int width, int height, Color backgroundColor)
+    {
+        Width = width;
+        Height = height;
+        BackgroundColor = backgroundColor;
+    }
+
+    public int Width { get; }
+    
+    public int Height { get; }
+    
+    public Color BackgroundColor { get; }
+}
