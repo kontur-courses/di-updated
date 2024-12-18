979976438053bfb63e4fa6b30384cc7ee47f7fc8
Ilfir Sakhabutdinov <ilfir.sakhabutdinov.study@mail.ru>
2024-12-18 18:02:44 +0500

    Add. Провайдер для изменения настроек анализа слов

---
+++ TagsCloudContainer.TextAnalyzer/Providers/Interfaces/IWordSettingsProvider.cs
@@ -0,0 +1,9 @@
+﻿using TagsCloudContainer.TextAnalyzer.Models;
+
+namespace TagsCloudContainer.TextAnalyzer.Providers.Interfaces;
+
+public interface IWordSettingsProvider
+{
+    public WordSettings GetWordSettings();
+    public void SetValidSpeechParts(IEnumerable<string> validSpeechParts);
+}
---
+++ TagsCloudContainer.TextAnalyzer/Providers/WordSettingsProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Frozen;
+using TagsCloudContainer.TextAnalyzer.Models;
+using TagsCloudContainer.TextAnalyzer.Providers.Interfaces;
+
+namespace TagsCloudContainer.TextAnalyzer.Providers;
+
+public class WordSettingsProvider : IWordSettingsProvider
+{
+    private WordSettings settings = new();
+
+    public WordSettings GetWordSettings() => settings;
+
+    public void SetValidSpeechParts(IEnumerable<string> validSpeechParts)
+    {
+        settings = settings with {ValidSpeechParts = validSpeechParts.ToFrozenSet()};
+    }
+}
