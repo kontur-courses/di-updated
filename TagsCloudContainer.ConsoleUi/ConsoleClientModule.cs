b036085962b8051e2f2f0097a1d5a2a330d714ae
Ilfir Sakhabutdinov <ilfir.sakhabutdinov.study@mail.ru>
2024-12-18 18:12:50 +0500

    Add. DI консольного клиента

---
+++ TagsCloudContainer.ConsoleUi/ConsoleClientModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using TagsCloudContainer.ConsoleUi.Handlers;
+using TagsCloudContainer.ConsoleUi.Handlers.Interfaces;
+using TagsCloudContainer.ConsoleUi.Runner;
+using TagsCloudContainer.ConsoleUi.Runner.Interfaces;
+
+namespace TagsCloudContainer.ConsoleUi;
+
+public class ConsoleClientModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<TagsCloudContainerUi>().As<ITagsCloudContainerUi>();
+        builder.RegisterType<ExitHandler>().As<IHandler>();
+        builder.RegisterType<WordSettingsHandler>().As<IHandler>();
+        builder.RegisterType<ImageSettingHandler>().As<IHandler>();
+        builder.RegisterType<VisualizationHandler>().As<IHandler>();
+    }
+}
---
+++ TagsCloudContainer.ConsoleUi/Program.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using TagsCloudContainer.ConsoleUi;
+using TagsCloudContainer.ConsoleUi.Runner.Interfaces;
+using TagsCloudContainer.TagsCloudVisualization;
+using TagsCloudContainer.TextAnalyzer;
+
+var builder = new ContainerBuilder();
+builder.RegisterModule(new TextAnalyzerModule());
+builder.RegisterModule(new TagsCloudVisualizationModule());
+builder.RegisterModule(new ConsoleClientModule());
+
+var app = builder.Build();
+using var scope = app.BeginLifetimeScope();
+var appRunner = scope.Resolve<ITagsCloudContainerUi>();
+appRunner.Run();
