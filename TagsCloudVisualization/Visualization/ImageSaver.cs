b55065927c44e3f673a3b39bdb632872ee626875
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-19 00:11:47 +0500

    Добавил класс сохраняющий изображение

---
+++ TagsCloudVisualization/Visualization/ImageSaver.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace TagsCloudVisualization.Visualization;
+
+public class ImageSaver
+{
+    public static void Save(Bitmap bitmap, string filePath, string fileName, ImageFormat imageFormat)
+    {
+        bitmap.Save(Path.Combine(filePath, fileName), imageFormat);
+    }
+}
