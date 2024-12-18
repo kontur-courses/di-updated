7506f8cc39d87f403561f77cfe70c6dc9933a911
Ilfir Sakhabutdinov <ilfir.sakhabutdinov.study@mail.ru>
2024-12-18 17:58:12 +0500

    Add. Провайдер для доставки и изменения настроек визуализации

---
+++ TagsCloudContainer.TagsCloudVisualization/Providers/ImageSettingsProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using TagsCloudContainer.TagsCloudVisualization.Models.Settings;
+using TagsCloudContainer.TagsCloudVisualization.Providers.Interfaces;
+
+namespace TagsCloudContainer.TagsCloudVisualization.Providers;
+
+public class ImageSettingsProvider : IImageSettingsProvider
+{
+    private ImageSettings imageSettings = new();
+
+    public ImageSettings GetImageSettings() =>
+        imageSettings;
+
+    public void SetSize(Size size) =>
+        imageSettings = imageSettings with {Size = size};
+
+    public void SetImageFormat(ImageFormat imageFormat) =>
+        imageSettings = imageSettings with {ImageFormat = imageFormat};
+
+    public void SetBackgroundColor(Color backgroundColor) =>
+        imageSettings = imageSettings with {BackgroundColor = backgroundColor};
+
+    public void SetWordColor(Color wordColor) =>
+        imageSettings = imageSettings with {WordColor = wordColor};
+
+    public void SetFontFamily(FontFamily fontFamily) =>
+        imageSettings = imageSettings with {FontFamily = fontFamily};
+}
