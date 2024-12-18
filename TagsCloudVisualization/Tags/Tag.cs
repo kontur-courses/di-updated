10b0ed0e0e4a9981a92ef0b7d6cae14e0dcb94eb
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-18 23:54:48 +0500

    Добавил интерфейс и класс тега

---
+++ TagsCloudVisualization/Tags/Tag.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization.Tags;
+
+public class Tag : ITag
+{
+    public Tag(string content, int size, Rectangle rectangle, FontFamily font)
+    {
+        Content = content;
+        Size = size;
+        Rectangle = rectangle;
+        Font = font;
+    }
+
+    public string Content { get; }
+    
+    public int Size { get; }
+    
+    public Rectangle Rectangle { get; }
+    
+    public FontFamily Font { get; }
+}
