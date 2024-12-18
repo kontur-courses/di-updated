49a3f7fcdee92871221a40cfad16dc9affa4b71f
Geratoptus <shura.bocharov.2004@mail.ru>
2024-12-18 18:05:08 +0500

    feat(SettingsFactory): добавил билдеры для новых ридеров и сейвера

--- TagCloudClient/SettingsFactory.cs
+++ TagCloudClient/SettingsFactory.cs
@@ -2,6 +2,7 @@ using TagCloud.CloudLayouter.PointLayouter.Generators;
 using TagCloud.CloudLayouter.PointLayouter.Settings;
 using TagCloud.CloudLayouter.PointLayouter.Settings.Generators;
 using TagCloud.ImageGenerator;
+using TagCloud.ImageSaver;
 using TagCloud.WordsReader.Settings;
 
 namespace TagCloudClient;
@@ -12,11 +13,20 @@ public static class SettingsFactory
         => new(options.Path, options.UsingEncoding);
 
     public static BitmapSettings BuildBitmapSettings(Options options)
-        => new(options.Size, options.Font, options.ImageName, options.BackgroundColor, options.WordsColor);
+        => new(options.Size, options.Font, options.BackgroundColor, options.ForegroundColor);
     
     public static FermatSpiralSettings BuildFermatSpiralSettings(Options options)
         => new(options.Radius, options.AngleOffset);
 
     public static PointLayouterSettings BuildPointLayouterSettings(Options options, IPointsGenerator generator)
         => new(options.Center, generator);
+    
+    public static WordFileReaderSettings BuildWordReaderSettings(Options options)
+        => new(options.Path);
+
+    public static CsvFileReaderSettings BuildCsvReaderSettings(Options options) 
+        => new(options.Path, options.Culture);
+
+    public static FileSaveSettings BuildFileSaveSettings(Options options)
+        => new(options.ImageName, options.ImageFormat);
 }
